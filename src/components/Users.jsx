9394fd2798f8436f19356bbe2e50ca45eec822af
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-19 01:11:36 +0530

    Update state after delete an item

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => { 
-    const users = useLoaderData();
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
     // sending a parameter to uniquely identify the item 
     const handleDelete = _id =>{
-            console.log('delete',_id);
+        console.log('delete',_id);
+        // create an dynamic url with dynamic id 
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: 'DELETE',
+
+        })
+        .then(res => res.json())
+        .then(data =>{
+            if(data.deletedCount > 0){
+                alert('Deleted successfully!');
+                // except the id of deleted item make an array of remaining items and set to the state 
+                const remaining = users.filter(user => user._id != _id);
+                setUsers(remaining);
+            }
+        })
     }
     return (
         <div className="mx-auto py-10 w-full px-20">
