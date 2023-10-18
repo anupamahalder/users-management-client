2ca805c0e12c32ea94c16c84c175a0ac44a5bbb9
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-18 13:55:09 +0530

    Create a object from user to send data to server using post api

--- src/App.jsx
+++ src/App.jsx
@@ -2,54 +2,46 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-
-  // declare a state to store users data 
+  // declare a state to hold user data 
   const [users, setUsers] = useState([]);
-  // load data 
+
+  // load data using useEffect hook for one time loading we will give and empty array dependency
   useEffect(()=>{
-    fetch('http://localhost:5000/users')
+    // the url will be the url where the users data is present in server 
+    fetch('http://localhost:5001/users')
     .then(res => res.json())
     .then(data => setUsers(data));
   },[]);
-  // function 
-  const handleAddUser = e =>{
-    e.preventDefault();
-    const form = e.target;
+
+  const handleAddUser = event =>{
+    event.preventDefault();
+    const form = event.target;
     const name = form.name.value;
     const email = form.email.value;
-    // we can create an object 
-    const user = {name,email};
-    // console.log(name,email);
-    console.log(user);
-    // send information as an object in an url using fetch method 
-    fetch('http://localhost:5000/users',{
-      method: 'POST',
-      headers: {
-        'content-type':'application/json',
-      },
-      // send data in body to make it stringified 
-      body: JSON.stringify(user)
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log('inside post response',data)
-    })
+    const password = form.password.value;
+    // create an object to have name, email and password as their property 
+    //  if both value and property name is same then we can write only once not required key value pair 
+    const newUser = {name, email, password};
+    console.log(newUser);
   }
+
   return (
     <>
       <h1>Users management system</h1>
-      <h3>Total number of users: {users.length}</h3>
+      <p>Total users: {users.length}</p>
+      {/* Now we want to send data to backend/server  */}
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='Enter your name' required autoComplete='username'/><br />
+        <input type="email" name='email' placeholder='Enter your email' required autoComplete='email'/><br />
+        <input type="password" name='password' placeholder='Enter your password' required autoComplete="off" /><br />
+        <input type="submit" value="Add User" />
+      </form>
       <div>
         {
-          users.map(user => <p key={user.id}>User name: {user.name}, email:{user.email}</p>)
+          users.map(user => <p key={user.id}>User ID: {user.id}, User name: {user.name},
+          User age: {user.age} </p>)
         }
       </div>
-      {/* send users data to the backend  */}
-      <form onSubmit={handleAddUser}>
-        <input type="text" name="name" placeholder='Enter name'/><br />
-        <input type="email" name="email" placeholder='Enter email' /><br />
-        <input type="submit" value="Add User" /><br />
-      </form>
     </>
   )
 }
