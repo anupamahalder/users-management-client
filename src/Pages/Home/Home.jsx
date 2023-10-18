1ddd33e6d58793b5f1b7cfb10e59ab1f6f615a55
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-18 20:42:12 +0530

    Create a home page to perform crud operation

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className="w-full h-screen">
+            <h1 className="py-10 text-center text-2xl text-blue-700 font-bold">User Management with CRUD operation</h1>
+        </div>
+    );
+};
+
+export default Home;
